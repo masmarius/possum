41cf806a22d13b08635c063572235c8e383942ef
masmarius <masmarius@users.noreply.github.com>
2024-12-02 15:15:17 -0600

    Update scoop-recipe.ps1

--- scoop-recipe.ps1
+++ scoop-recipe.ps1
@@ -2,3 +2,6 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 scoop install 7zip innounp dark sudo git
 sudo Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
+~\scoop\apps\7zip\current\install-context.reg
+git config --global credential.helper manager
+~\scoop\apps\git\current\install-context.reg
